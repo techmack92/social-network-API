3c67dc035f5ccd5d88f56ba57651d5e007f2a17d
Mack <mldixon9750@gmail.com>
2024-02-05 16:23:52 -0800

    added routes in userRoutes.js

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUserById,
+    deleteUserById,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router
+.route('/')
+.get(getUsers)
+.post(createUser);
+
+// /api/users/:userId
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUserById)
+.delete(deleteUserById);
+
+// /api/users/:userId/friends/:friendId
+router
+.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
