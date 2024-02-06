1e941efc672ad6b2c094050c499cb02c7af0d917
Mack <mldixon9750@gmail.com>
2024-02-05 16:04:48 -0800

    added functions to get users by ID, update and delete users, add and delete friends to userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,19 @@ async function getUsers(req, res) {
     }
 };
 
+async function getUserById(req, res) {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(400).json({ message: 'No users with that ID⚠️' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
 async function createUser(req, res) {
     try {
         const user = await User.create(req.body);
@@ -19,6 +32,94 @@ async function createUser(req, res) {
     }
 };
 
-// async function getUserById(req, res) {
-//     const 
-// }
+async function updateUserById(req, res) {
+    const { id } = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(
+            id,
+            {
+                $set: req.body
+            },
+            {
+                runValidators: true,
+                new: true
+            },
+        );
+        if (!user) {
+            return res.status(404).json({ message: 'No users with that ID⚠️' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+async function deleteUserById(req, res) {
+    const { id } = req.params;
+    try {
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+            return res.status(404).json({ message: 'No users with that ID⚠️' });
+        }
+        await Thought.deleteMany({ username: user.username });
+        res.json({ message: 'User deleted🚮', user });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+async function addFriend(req, res) {
+    const { userId, friendId } = req.params;
+    try {
+        const friend = await User.findByIdAndUpdate(userId, 
+            { $addToSet: 
+                { 
+                    friends: friendId
+                } 
+            }, 
+            { 
+                runValidators: true, 
+                new: true
+            }
+        );
+        if (!friend) {
+            return res.status(404).json({ message: 'No users with that ID⚠️' });
+        }
+        res.json({ message: 'New friend added🆕', friend });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+async function deleteFriend(req, res) {
+    const { userId, friendId } = req.params;
+    try {
+        const friend = await User.findByIdAndUpdate(userId, 
+            { $pull: 
+                { 
+                    friends: friendId
+                } 
+            }, 
+            { 
+                runValidators: true, 
+                new: true
+            }
+        );
+        if (!friend) {
+            return res.status(404).json({ message: 'No users with that ID⚠️' });
+        }
+        res.json({ message: 'Friend has been removed✅', friend });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUserById,
+    deleteUserById,
+    addFriend,
+    deleteFriend
+};
